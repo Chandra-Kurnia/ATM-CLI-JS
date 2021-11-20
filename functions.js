11e2752339a1714940ba0edd542769c50a16e6bc
Chandra_Kurnia <candrakurniawan.dev@gmail.com>
2021-11-20 21:10:03 +0700

    transfer command

--- functions.js
+++ functions.js
@@ -1,12 +1,18 @@
 const login = (name) => {
   console.log('Welcome ' + name);
+
 };
 
 const deposit = (amount) => {
     console.log('Deposit ' + amount);
 };
 
+const transfer = (target, amount) => {
+    console.log(`You transfered to ${target} with amount = ${amount}`);
+}
+
 module.exports = {
   login,
-  deposit
+  deposit,
+  transfer
 };
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const program = require('commander');
-const {
-    login,
-    deposit
-} = require('./functions');
+const {login, deposit, transfer} = require('./functions');
 
 program.version('1.0.0').description('ATM CLI');
 
@@ -15,11 +12,19 @@ program
   });
 
 program
-.command('deposit <amount>')
-.alias('D')
-.description('Deposit to account')
-.action(amount => {
-    deposit(amount)
-});
+  .command('deposit <amount>')
+  .alias('D')
+  .description('Deposit to account')
+  .action((amount) => {
+    deposit(amount);
+  });
+
+program
+  .command('transfer <target> <amount>')
+  .alias('TF')
+  .description('Transfer to other user')
+  .action((target, amount) => {
+    transfer(target, amount)
+  })
 
 program.parse(process.argv);
